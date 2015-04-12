160025e0644aeede25361c6cd7ed5eb8cb8bebbc
Meinrad Jean-Richard <mjr@jean-richard.ch>
2015-04-12 11:06:15 +0200

    Animate Tiles on loaded.

--- WinRtMap/Tiles/WebTile.cs
+++ WinRtMap/Tiles/WebTile.cs
@@ -6,6 +6,7 @@ using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace WinRtMap.Tiles
@@ -27,6 +28,7 @@ namespace WinRtMap.Tiles
 			Image image = new Image();
 			image.IsHitTestVisible = false;
 			image.Width = 256;
+			image.Opacity = 0;
 			image.Height = 256;
 			image.Stretch = Stretch.None;
 			_bitmap = new BitmapImage();
@@ -57,8 +59,25 @@ namespace WinRtMap.Tiles
 
 		public async Task SetImage(IRandomAccessStream imageStream)
 		{
-			await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { _bitmap.SetSource(imageStream); });
+			await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => SetImageSource(imageStream));
 			HasImage = true;
+		}
+
+		private void SetImageSource(IRandomAccessStream imageStream)
+		{
+			_bitmap.SetSource(imageStream);
+			AnimateTile();
+		}
+
+		protected virtual void AnimateTile()
+		{
+			DoubleAnimation doubleAnimation = new DoubleAnimation {To = 1d, Duration = TimeSpan.FromMilliseconds(500)};
+			doubleAnimation.EasingFunction = new CubicEase();
+			Storyboard.SetTargetProperty(doubleAnimation, "Opacity");
+			Storyboard.SetTarget(doubleAnimation, _image);
+			var storyboard = new Storyboard();
+			storyboard.Children.Add(doubleAnimation);
+			storyboard.Begin();
 		}
 	}
 }
